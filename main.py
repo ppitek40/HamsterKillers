416d751efcdcdbc387f0f42ba0110c16a1175048
Pitek <ppitek40@o2.pl>
2021-01-05 00:09:33 +0100

    BugFixes and adding messages from processes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+from enum import Enum
 from mpi4py import MPI
 import pydevd_pycharm
 
@@ -10,6 +11,15 @@ def enum(*sequential, **named):
     """
     enums = dict(zip(sequential, range(len(sequential))), **named)
     return type('Enum', (), enums)
+
+class tags(Enum):
+    ZLECENIE_ZAPYTANIE=1
+    ZLECENIE_ZEZWOLENIE=2
+    ZLECENIA=3
+    AGRAFKA_ZAPYTANIE=4
+    AGRAFKA_ZEZWOLENIE=5
+    ZEZWOLENIA_INNE=6
+    KONIEC=7
 
 
 def generateTasks():
@@ -51,8 +61,7 @@ def takePoisonAndKillHamsters():
 
 
 def main():
-    tags = enum('ZLECENIE_ZAPYTANIE', 'ZLECENIE_ZEZWOLENIE', 'ZLECENIA', 'AGRAFKA_ZAPYTANIE', 'AGRAFKA_ZEZWOLENIE',
-                'ZEZWOLENIA_INNE', 'KONIEC')
+    #tags = Enum('ZLECENIE_ZAPYTANIE', 'ZLECENIE_ZEZWOLENIE', 'ZLECENIA', 'AGRAFKA_ZAPYTANIE', 'AGRAFKA_ZEZWOLENIE', 'ZEZWOLENIA_INNE', 'KONIEC')
     status = MPI.Status()
     numberOfSessions = 2
     session = 1
@@ -67,19 +76,19 @@ def main():
         tasks = generateTasks()
         doneTasks = 0
         print(tasks)
-        sendToAll(comm, rank, size, tasks, tags.ZLECENIA)
+        sendToAll(comm, rank, size, [tasks,0], tags.ZLECENIA.value)
+        print('ID:'+str(rank)+' | 0 | Wyslalem do wszystkich liste zlecen', flush=True, )
 
         # wait4Answers
 
         while doneTasks < len(tasks):
-            data = comm.recv(dest=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
             if status.Get_tag() == tags.KONIEC:
                 doneTasks += 1
 
-        sendToAll(comm, rank, size, 0, tags.KONIEC)
+        sendToAll(comm, rank, size, 0, tags.KONIEC.value)
 
         session += 1
-
     else:
         tasks = None
         currentTask = None
@@ -89,19 +98,31 @@ def main():
         timeOfSafetyPinRequest = 0
         numberOfConsents = 0
         wantSafetyPin = False
-        SafetyPinRequests = []
+        safetyPinRequests = []
         end = False
 
         while not end:
 
             data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            tag = tags(status.Get_tag())
+            print('ID:' + str(rank) + ' | '+str(time)+
+                  ' | Otrzymalem wiadomosc typu: '+ tag.name +' od procesu ID: '
+                  +str(status.Get_source())+' | czas wysłania: '+str(data[-1]), flush=True, )
+
             time = max(time, data[-1]) + 1
-            tag = status.Get_tag()
+            print('ID:' + str(rank) + ' | ' + str(time) +
+                  ' | Aktualizacja czasu', flush=True, )
 
             if tag == tags.ZLECENIE_ZAPYTANIE:
-                if currentTask == data:
+                if currentTask == data[0]:
                     if war(timeOfTaskRequest, data[-1], rank, status.Get_source()):
+                        print('ID:' + str(rank) + ' | ' + str(time) +
+                              ' | Wygrałem wojnę o zadanie '+str(currentTask)+
+                              ' z procesem o ID: '+str(status.Get_source()), flush=True, )
                         continue
+                    print('ID:' + str(rank) + ' | ' + str(time) +
+                          ' | Przegrałem wojnę o zadanie ' + str(currentTask) +
+                          ' z procesem o ID: ' + str(status.Get_source()), flush=True, )
                     task = chooseTask(tasks)
                     if task < 0:
                         continue
@@ -109,16 +130,16 @@ def main():
                     time += 1
                     timeOfTaskRequest = time
                     numberOfConsents=0
-                    sendToAll(comm, rank, size, task, tags.ZLECENIE_ZAPYTANIE)
+                    sendToAll(comm, rank, size, [task,time], tags.ZLECENIE_ZAPYTANIE.value)
 
-                if tasks[data][1]:
+                if tasks[data[0]][1]:
                     continue
-                tasks[data][1] = True
+                tasks[data[0]][1] = True
                 time += 1
-                comm.send([data, time], dest=status.Get_source(), tag=tags.ZLECENIE_ZEZWOLENIE)
+                comm.send([data[0], time], dest=status.Get_source(), tag=tags.ZLECENIE_ZEZWOLENIE.value)
 
             elif tag == tags.ZLECENIE_ZEZWOLENIE:
-                if data == currentTask:
+                if data[0] == currentTask:
                     numberOfConsents += 1
                     if numberOfConsents < size - 2:
                         continue
@@ -128,23 +149,23 @@ def main():
                         numberOfConsents = 0
                         time += 1
                         timeOfSafetyPinRequest = time
-                        sendToAll(comm, rank, size, 1, tags.AGRAFKA_ZAPYTANIE)
+                        sendToAll(comm, rank, size, [1,time], tags.AGRAFKA_ZAPYTANIE.value)
                 else:
                     dest = [x[1] for x in LostTasks if x[0] == data]
                     time += 1
-                    comm.send([data, 1, time], dest=dest, tag=tags.ZEZWOLENIA_INNE)
+                    comm.send([data[0], 1, time], dest=dest, tag=tags.ZEZWOLENIA_INNE.value)
             # SPRAWDZIC CZY NA PEWNO JEST GIT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             # TEST
             elif tag == tags.AGRAFKA_ZAPYTANIE:
                 if wantSafetyPin:
                     if war(timeOfSafetyPinRequest, data[-1], rank, status.Get_source()):
-                        SafetyPinRequests.append(status.Get_source())
+                        safetyPinRequests.append(status.Get_source())
                     else:
                         time+=1
-                        comm.send([1,time], dest=status.Get_source(), tag=tags.AGRAFKA_ZEZWOLENIE)
+                        comm.send([1,time], dest=status.Get_source(), tag=tags.AGRAFKA_ZEZWOLENIE.value)
                 else:
                     time += 1
-                    comm.send([1,time], dest=status.Get_source(), tag=tags.AGRAFKA_ZEZWOLENIE)
+                    comm.send([1,time], dest=status.Get_source(), tag=tags.AGRAFKA_ZEZWOLENIE.value)
 
             elif tag == tags.AGRAFKA_ZEZWOLENIE:
                 if not wantSafetyPin:
@@ -156,8 +177,8 @@ def main():
                 numberOfConsents = 0
                 takePoisonAndKillHamsters()
                 time+=1
-                for x in SafetyPinRequests:
-                    comm.send([1,time], dest=x,tag=tags.AGRAFKA_ZEZWOLENIE)
+                for x in safetyPinRequests:
+                    comm.send([1,time], dest=x,tag=tags.AGRAFKA_ZEZWOLENIE.value)
 
 
             elif tag == tags.ZEZWOLENIA_INNE:
@@ -171,15 +192,15 @@ def main():
                         numberOfConsents = 0
                         time += 1
                         timeOfSafetyPinRequest = time
-                        sendToAll(comm, rank, size, [1,time], tags.AGRAFKA_ZAPYTANIE)
+                        sendToAll(comm, rank, size, [1,time], tags.AGRAFKA_ZAPYTANIE.value)
                 else:
                     dest = [x[1] for x in LostTasks if x[0] == data]
                     time += 1
-                    comm.send([data, 1, time], dest=dest, tag=tags.ZEZWOLENIA_INNE)
+                    comm.send([data[0], 1, time], dest=dest, tag=tags.ZEZWOLENIA_INNE.value)
             # POPRAWIC!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
             elif tag == tags.ZLECENIA:
-                tasks = [[x, False] for x in data]
+                tasks = [[x, False] for x in data[0]]
                 task = chooseTask(tasks)
                 if task < 0:
                     continue
@@ -187,7 +208,7 @@ def main():
                 time += 1
                 timeOfTaskRequest = time
                 numberOfConsents=0
-                sendToAll(comm, rank, size, [task, time], tags.ZLECENIE_ZAPYTANIE)
+                sendToAll(comm, rank, size, [task, time], tags.ZLECENIE_ZAPYTANIE.value)
 
             elif tag == tags.KONIEC:
                 end = True
