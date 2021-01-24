59d2f9547460dd04300f728ec2638abc89aff82d
Pitek <ppitek40@o2.pl>
2021-01-24 12:29:07 +0100

    bugfix

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def chooseTask(tasks, time, comm):
     if False in list(zip(*tasks))[1]:
         number = random.randint(0, len(tasks) - 1)
         if tasks[number][1]:
-            number = (zip(*tasks))[1].index(False)
+            number = list(zip(*tasks))[1].index(False)
         time += 1
         Logger(5, [comm.Get_rank(), time, number, Tags.ZLECENIE_ZAPYTANIE.name])
         sendToAll(comm, comm.Get_rank(), comm.Get_size(), [number, time], Tags.ZLECENIE_ZAPYTANIE.value)
